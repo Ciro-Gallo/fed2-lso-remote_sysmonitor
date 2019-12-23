d29766669a6b8aa63f9a7b605bb52de9207cb97f
Ciro-Gallo <ciro.gallo.sw@gmail.com>
2019-12-23 11:17:12 +0100

    added client and agent stub

--- server.c
+++ server.c
@@ -4,6 +4,7 @@
 //GLOBAL VARIABLES
 node * sdContainer;
 int sdAgent;
+int sdClient;
 pthread_attr_t threadAttributes;
 bool serverKilled = false;
 BSTHostInfo * bstHostInfo;
@@ -109,6 +110,156 @@ void initBSTHostInfo(void){
     pthread_mutex_init(&bstHostInfo->mutex,NULL);
 }
 
+void * handleClient(void * arg){
+    int socketClient = *(int *)arg;
+
+    
+}
+
+void * handleClientStub(void * arg){
+    pthread_t tid_client;
+
+    struct sockaddr_in client_addr;
+    
+
+    socklen_t size_client_addr = sizeof(client_addr);
+    
+    struct hostent * clientInfo;
+    struct in_addr inClientAddress;
+
+    pthread_t tid;
+    int sdClient; 
+
+    char * idClient;
+    char * instant;
+    char * clientIP;
+
+    time_t timer;
+    agentInfo * info;
+
+    while(1){
+
+        sdClient = accept(sdAgent,(struct sockaddr *)&client_addr,&size_client_addr);
+
+        if(sdClient != -1){
+/*
+            //Get time
+            time(&timer);
+            instant = ctime(&timer);
+            
+            //Get hostname or IP (if host not available)
+            inClientAddress = client_addr.sin_addr;
+
+            printf("\nClient IP before resolution: %s\n", inet_ntoa(client_addr.sin_addr));
+
+            clientInfo = gethostbyaddr(&inClientAddress,sizeof(inClientAddress),AF_INET);
+
+            //Get agent's IP as string
+            clientIP = (char *)malloc(sizeof(char)*(strlen(inet_ntoa(client_addr.sin_addr))+1));
+            strcpy(clientIP,inet_ntoa(client_addr.sin_addr));
+
+            if(clientInfo != NULL){
+                idClient = (char *)malloc(sizeof(char)*(strlen(clientInfo->h_name)+1));
+                strcpy(idClient,clientInfo->h_name);
+            }
+            else{
+                //error("gethostfun",h_errno);
+                idClient = (char *)malloc(sizeof(char)*(strlen(clientIP)+1));
+                strcpy(idClient,clientIP);
+                printf("Resolution failed. IP: %s\n", idClient);
+            }
+
+            //Prepare struct to pass to thread
+            info = (agentInfo *)malloc(sizeof(agentInfo));
+
+            info->sd = sdClient;
+            info->time = instant;
+            info->idhost = idClient;
+            info->IP = clientIP;
+*/
+            pthread_create(&tid,&threadAttributes,handleClient,&sdClient);
+            
+            listInsert(sdContainer,sdClient,tid);
+        }
+        else{
+            error("Accept error!",1);
+        }
+    }
+}
+
+void * handleAgentStub(void * arg){
+
+    pthread_t tid_agent;
+
+    struct sockaddr_in agent_addr;
+    
+
+    socklen_t size_agent_addr = sizeof(agent_addr);
+    
+
+    struct hostent * clientInfo;
+    struct in_addr inAgentAddress;
+
+    pthread_t tid;
+    int sdAgent2;
+
+    char * idAgent;
+    char * instant;
+    char * agentIP;
+
+    time_t timer;
+    agentInfo * info;
+
+    while(1){
+
+        sdAgent2 = accept(sdAgent,(struct sockaddr *)&agent_addr,&size_agent_addr);
+
+        if(sdAgent2 != -1){
+
+            //Get time
+            time(&timer);
+            instant = ctime(&timer);
+            
+            //Get hostname or IP (if host not available)
+            inAgentAddress = agent_addr.sin_addr;
+
+            printf("\nAgent IP before resolution: %s\n", inet_ntoa(agent_addr.sin_addr));
+
+            clientInfo = gethostbyaddr(&inAgentAddress,sizeof(inAgentAddress),AF_INET);
+
+            //Get agent's IP as string
+            agentIP = (char *)malloc(sizeof(char)*(strlen(inet_ntoa(agent_addr.sin_addr))+1));
+            strcpy(agentIP,inet_ntoa(agent_addr.sin_addr));
+
+            if(clientInfo != NULL){
+                idAgent = (char *)malloc(sizeof(char)*(strlen(clientInfo->h_name)+1));
+                strcpy(idAgent,clientInfo->h_name);
+            }
+            else{
+                //error("gethostfun",h_errno);
+                idAgent = (char *)malloc(sizeof(char)*(strlen(agentIP)+1));
+                strcpy(idAgent,agentIP);
+                printf("Resolution failed. IP: %s\n", idAgent);
+            }
+
+            //Prepare struct to pass to thread
+            info = (agentInfo *)malloc(sizeof(agentInfo));
+
+            info->sd = sdAgent2;
+            info->time = instant;
+            info->idhost = idAgent;
+            info->IP = agentIP;
+
+            pthread_create(&tid,&threadAttributes,handleAgent,info);
+            
+            listInsert(sdContainer,sdAgent2,tid);
+        }
+        else{
+            error("Accept error!",1);
+        }
+    }
+}
+
 void * handleAgent(void * arg){
     agentInfo * info = (agentInfo *)arg;
 
@@ -207,25 +358,38 @@ int main(int argc, char * argv[]){
     //Redirect stderr to stdout
     dup2(STDOUT_FILENO,STDERR_FILENO);
 
-    struct sockaddr_in server_addr, client_addr;
-
     if(argv[1]==NULL){
-        printf("usage: %s <port>\n", argv[0]);
+        printf("usage: %s <port_agent> <port_client>\n", argv[0]);
         exit(1);
     }
     
-    int port = parseInt(argv[1]);
+    int port_agent = parseInt(argv[1]);
 
-    if(port<MIN_PORT || port>MAX_PORT){ //Fallita la conversione o porta non compresa nel range
-        printf("usage: %s <port>. Insert valid port!\n", argv[0]);
+    if(port_agent<MIN_PORT || port_agent>MAX_PORT){ //Fallita la conversione o porta non compresa nel range
+        printf("usage: %s <port_agent> <port_client>. Insert valid port!\n", argv[0]);
         exit(1);
     }
 
-    printf("\nServer listening on port %d...\n\n", port);
+    int port_client = parseInt(argv[2]);
+
+    if(port_client<MIN_PORT || port_client>MAX_PORT){ //Fallita la conversione o porta non compresa nel range
+        printf("usage: %s <port_agent> <port_client>. Insert valid port!\n", argv[0]);
+        exit(1);
+    }
+
+    printf("\nServer listening on port %d...\n\n", port_agent);
+
+    //Declaring variables
+    struct sockaddr_in server_addr, agent_addr;
+    struct sockaddr_in server_addr_client, client_addr;
 
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(port);
+    server_addr.sin_port = htons(port_agent);
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    server_addr_client.sin_family = AF_INET;
+    server_addr_client.sin_port = htons(port_client);
+    server_addr_client.sin_addr.s_addr = htonl(INADDR_ANY);
 
     sdAgent = socket(PF_INET,SOCK_STREAM,0);
     
@@ -240,6 +404,20 @@ int main(int argc, char * argv[]){
         error("Error in listening!\n",1);
     }
 
+    sdClient = socket(PF_INET,SOCK_STREAM,0);
+    
+    if(sdClient == -1)
+        error("Error creating the socket!\n",1);
+
+    if(bind(sdClient,(struct sockaddr *)&server_addr_client,sizeof(server_addr_client)) == -1){
+        error("Error binding address!\n",1);
+    }
+
+    if(listen(sdClient,5) == -1){
+        error("Error in listening!\n",1);
+    }
+
+
     if(pthread_attr_init(&threadAttributes) != 0){
         exit(-1);
     }
@@ -247,71 +425,22 @@ int main(int argc, char * argv[]){
         exit(-1);
     }
 
+
     //Init struct containing mutex and bst root
     initBSTHostInfo();
     //Create list cointaining sd and tid
     sdContainer = listCreate();
 
-    socklen_t size_client_addr = sizeof(client_addr);
-    pthread_t tid;
+    pthread_t tid_agent, tid_client;
 
-    struct hostent * clientInfo;
-    struct in_addr inAgentAddress;
+    //Creating stub thread for agent
+    if(pthread_create(&tid_agent,&threadAttributes,handleAgentStub,NULL) != 0){
+        error("Error creating stub thread for agent\n",-3);
+    }
 
-    int sdAgent2;
-
-    char * idAgent;
-    char * instant;
-    char * agentIP;
-
-    time_t timer;
-
-    while(1){
-
-        sdAgent2 = accept(sdAgent,(struct sockaddr *)&client_addr,&size_client_addr);
-
-        if(sdAgent2 != -1){
-
-            //Get time
-            time(&timer);
-            instant = ctime(&timer);
-            
-            //Get hostname or IP (if host not available)
-            inAgentAddress = client_addr.sin_addr;
-
-            printf("\nAgent IP before resolution: %s\n", inet_ntoa(client_addr.sin_addr));
-
-            clientInfo = gethostbyaddr(&inAgentAddress,sizeof(inAgentAddress),AF_INET);
-
-            //Get agent's IP as string
-            agentIP = (char *)malloc(sizeof(char)*(strlen(inet_ntoa(client_addr.sin_addr))+1));
-            strcpy(agentIP,inet_ntoa(client_addr.sin_addr));
-
-            if(clientInfo != NULL){
-                idAgent = (char *)malloc(sizeof(char)*(strlen(clientInfo->h_name)+1));
-                strcpy(idAgent,clientInfo->h_name);
-            }
-            else{
-                //error("gethostfun",h_errno);
-                idAgent = (char *)malloc(sizeof(char)*(strlen(agentIP)+1));
-                strcpy(idAgent,agentIP);
-                printf("Resolution failed. IP: %s\n", idAgent);
-            }
-
-            //Prepare struct to pass to thread
-            agentInfo * info = (agentInfo *)malloc(sizeof(agentInfo));
-            info->sd = sdAgent2;
-            info->time = instant;
-            info->idhost = idAgent;
-            info->IP = agentIP;
-
-            pthread_create(&tid,&threadAttributes,handleAgent,info);
-            
-            listInsert(sdContainer,sdAgent2,tid);
-        }
-        else{
-            error("Accept error!",1);
-        }
+    //Creating stub thread for client
+    if(pthread_create(&tid_client,&threadAttributes,handleClientStub,NULL) != 0){
+        error("Error creating stub thread for client\n",-3);
     }
 
 }
